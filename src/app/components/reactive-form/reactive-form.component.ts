7629ee5f6ef9705e46f7674217b1f48be25346e4
redluck <redluck@tiscali.it>
2018-12-23 21:44:04 +0100

    Aggiornamento

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Contact } from '../../models/contact';
